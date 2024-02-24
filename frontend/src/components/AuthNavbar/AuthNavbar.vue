908ccf86c5816dd91ecc39f03ab8b584fd31b333
Indrajit Pal <palindrajit10@gmail.com>
2024-02-24 22:47:40 +0530

    .

--- frontend/src/components/AuthNavbar/AuthNavbar.vue
+++ frontend/src/components/AuthNavbar/AuthNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Library Management System</a>
+      <router-link to="/" class="navbar-brand">Library Management System</router-link>
     </div>
   </nav>
 </template>
